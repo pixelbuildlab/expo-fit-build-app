1de7d7089c11ed28c6f562ff7c79bddf6f43a9e5
pixelbuildlab <2017n2801@gmail.com>
2025-08-24 07:47:03 +0500

    babel env setup

--- expo-app/src/app/_layout.tsx
+++ expo-app/src/app/_layout.tsx
@@ -6,43 +6,12 @@ import {tokenCache} from '@clerk/clerk-expo/token-cache';
 import {StatusBar} from 'expo-status-bar';
 
 export default function Layout() {
+  console.log(process.env.APP_API_URL, 'api');
   return (
     <ClerkProvider tokenCache={tokenCache}>
       <StatusBar style="dark" />
       <Slot />
       <Toast topOffset={60} />
     </ClerkProvider>
-    // <Tabs>
-    //   <Tabs.Screen
-    //     name="index"
-    //     options={{
-    //       title: 'Home',
-    //       headerShown: false,
-    //       tabBarIcon: ({color, size}) => (
-    //         <AntDesign name="home" color={color} size={size} />
-    //       ),
-    //     }}
-    //   />
-    //   <Tabs.Screen
-    //     name="profile"
-    //     options={{
-    //       title: 'Profile',
-    //       headerShown: false,
-    //       tabBarIcon: ({color, size}) => (
-    //         <AntDesign name="user" color={color} size={size} />
-    //       ),
-    //     }}
-    //   />
-    //   <Tabs.Screen
-    //     name="history"
-    //     options={{
-    //       title: 'History',
-    //       headerShown: false,
-    //       tabBarIcon: ({color, size}) => (
-    //         <AntDesign name="clockcircle" color={color} size={size} />
-    //       ),
-    //     }}
-    //   />
-    // </Tabs>
   );
 }
